14ef47e0289b618a8ff4bacc6844b96d0826ed21
MargaritaWang <112512721+Margaritajellyfish@users.noreply.github.com>
2022-10-11 02:42:02 +0000

    7-7

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 
 num1 = list(map(int, input().split()))
 num2 = list(map(int, input().split()))
+num3 =[]
+if len(num2) > len(num1):
+    long = num2
+else : long = num1
+for i in range(len(long)):
+    try: num3.append(num1[i])
+    except: continue
+    try: num3.append(num2[i])
+    except: continue
+print(num3)
 
 # ******************************
 # Make your Code
